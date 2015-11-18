19f26038d3012530b244b5e33cdea7b287b85ec4
IvanYang-BJ <125781809@qq.com>
2015-11-18 11:35:46 +0800

    Test Behavior Finish!

--- Application/Home/Behavior/TestBehavior.class.php
+++ Application/Home/Behavior/TestBehavior.class.php
@@ -12,8 +12,8 @@ use Think\Behavior;
 class TestBehavior
 {
     public function run(&$params){
-        if(C('DATABASE_NAME')) {
-            echo 'RUNTEST BEHAVIOR '.$params.BR;
+        if(C('DATABASE_USERNAME')) {
+            echo 'RUNTEST BEdfadsfadsHAVIOR '.$params.BR;
         }
         else{
             echo 'Run Test Behavior No Params'.BR;
