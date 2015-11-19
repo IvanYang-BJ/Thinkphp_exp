8c4b382bd313d549f784fcc4be9298f4ad8cb19b
IvanYang-BJ <125781809@qq.com>
2015-11-19 13:53:24 +0800

    控制器->Action参数绑定

---
+++ Application/Home/Controller/BlogController.class.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: egret
+ * Date: 2015/11/19
+ * Time: 13:31
+ */
+
+namespace Home\Controller;
+
+use Think\Controller;
+define('BR','</br>');
+class BlogController extends Controller
+{
+    //http://localhost/thinkphp_self/home.php/blog/read/id/2
+    public function read($id=0){
+        echo 'id = '.$id.BR;
+    }
+    //http://localhost/thinkphp_self/home.php/blog/archive/year/1000/month/10
+    public function archive($year=2015,$month=11){
+        echo 'year = '.$year.BR;
+        echo 'month = '.$month.BR;
+    }
+}
